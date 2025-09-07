7c73d5eaf7f8afa9e2035bfab0fefc05b84eb027
vblaws <h3239498031@gmail.com>
2025-09-07 14:41:11 +0800

    feat(dish): 新增菜品分类查询功能
    - 在 DishController 中添加菜品分类查询接口
    - 在 DishService 接口中定义 pageQuery 方法
    - 实现 DishServiceImpl 中的 pageQuery 方法，使用 PageHelper 进行分页查询
    - 在 DishMapper 中添加 queryPages 方法，用于查询菜品信息
    - 更新 DishMapper.xml，添加新的 SQL 查询语句

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -1,16 +1,18 @@
 package com.sky.controller.admin;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.entity.Dish;
+import com.sky.mapper.DishMapper;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.DishService;
+import com.sky.vo.DishVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 菜品管理
@@ -22,12 +24,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class DishController {
     @Autowired
     public DishService dishService;
+    private DishMapper dishMapper;
+    
     @PostMapping
     @ApiOperation(value = "新增菜品")
     public Result save(@RequestBody DishDTO dishDTO){
         log.info("新增菜品{}",dishDTO);
         dishService.saveWithFlavor(dishDTO);
         return Result.success();
+    }
+    
+    @GetMapping("/page")
+    @ApiOperation(value = "菜品分类查询")
+    public Result<PageResult> queryPages(DishPageQueryDTO dishPageQueryDTO){//作为普通参数
+        log.info("菜品分类查询{}",dishPageQueryDTO);
+        
+        PageResult pageResult = dishService.pageQuery(dishPageQueryDTO);
+        
+        return Result.success(pageResult);
+        
     }
 
 
--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,9 +1,12 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 
 public interface DishService {
     //新增菜品和口味数据
     void saveWithFlavor(DishDTO dishDTO);
     
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
