8946f7c4c05890c52f285f498e43b67d3f6c63d3
vblaws <h3239498031@gmail.com>
2025-09-14 11:43:50 +0800

    feat(admin): 添加套餐相关接口并优化数据插入
    - 在 SetmealController 中添加了套餐相关接口- 在 SetmealDishMapper 中添加了插入操作的自动填充注解
    - 更新了 SetmealMapper.xml 中的插入语句，增加了创建和更新相关的字段

--- sky-server/src/main/java/com/sky/controller/admin/setmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/setmealController.java
@@ -10,7 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/admin/setmeal")
 @Api(tags = "套餐相关接口")
 @RequiredArgsConstructor
 @Slf4j
--- sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
@@ -1,6 +1,8 @@
 package com.sky.mapper;
 
+import com.sky.annotation.AutoFill;
 import com.sky.entity.SetmealDish;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -10,6 +12,6 @@ import java.util.List;
 public interface SetmealDishMapper {
     //查询菜品id对应套餐id
     List<Long> getSetmealIdsByDishIds(List<Long> ids);
-    
+    @AutoFill(OperationType.INSERT)
     void insertBatch(List<SetmealDish> setmealDishes);
 }
