7dc4e7ef1e7e5dd51abb7a111860abfb5c02d6ac
vblaws <h3239498031@gmail.com>
2025-10-27 19:44:02 +0800

    feat(order): 实现订单提交基础逻辑
    - 新增地址簿和购物车数据校验
    - 添加订单和订单详情实体类属性拷贝
    - 集成BaseContext获取当前用户ID
    - 抛出地址簿和购物车业务异常
    - 初始化订单提交VO返回对象

--- sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -1,11 +1,21 @@
 package com.sky.service.impl;
 
+import com.sky.constant.MessageConstant;
+import com.sky.context.BaseContext;
 import com.sky.dto.OrdersSubmitDTO;
+import com.sky.entity.Orders;
+import com.sky.entity.ShoppingCart;
+import com.sky.exception.AddressBookBusinessException;
+import com.sky.exception.ShoppingCartBusinessException;
+import com.sky.mapper.AddressBookMapper;
 import com.sky.mapper.OrderDetailMapper;
 import com.sky.mapper.OrderMapper;
+import com.sky.mapper.ShoppingCartMapper;
 import com.sky.service.OrderService;
 import com.sky.vo.OrderSubmitVO;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.bridge.Message;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,11 +23,30 @@ import org.springframework.stereotype.Service;
 public class OrderServiceImpl implements OrderService {
     private final OrderMapper orderMapper;
     private final OrderDetailMapper orderDetailMapper;
-    
+    private final AddressBookMapper addressBookMapper;
+    private final ShoppingCartMapper shoppingCartMapper;
     @Override
     public OrderSubmitVO submitOrder(OrdersSubmitDTO ordersSubmitDTO) {
         
+        // 处理业务异常 1. 收货地址为空 2. 购物车数据为空
+        if(addressBookMapper.getById(ordersSubmitDTO.getAddressBookId()) == null)
+            throw new AddressBookBusinessException(MessageConstant.ADDRESS_BOOK_IS_NULL);
         
+        ShoppingCart cart = new ShoppingCart();
+        Long userId= BaseContext.getCurrentId();
+        cart.setUserId(userId);
+        
+        if(shoppingCartMapper.list(cart) == null && shoppingCartMapper.list(cart).size() > 0)
+            throw new ShoppingCartBusinessException(MessageConstant.SHOPPING_CART_IS_NULL);
+        // 向订单表插入一条数据
+        Orders orders = new Orders();
+        
+        BeanUtils.copyProperties(ordersSubmitDTO,orders);
+        //向订单详细表插入多条数据
+        
+        //清空购物车
+        
+        //封装VO返回结果
         return null;
     }
 }
