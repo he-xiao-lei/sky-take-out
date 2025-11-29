440f72d4a8dfac75ca6395e0c132690554688f52
vblaws <h3239498031@gmail.com>
2025-11-29 16:09:58 +0800

    feat(order): 查看订单详情

--- sky-server/src/main/java/com/sky/controller/admin/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/admin/OrderController.java
@@ -5,11 +5,13 @@ import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.OrderService;
 import com.sky.vo.OrderStatisticsVO;
+import com.sky.vo.OrderVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,5 +35,11 @@ public class OrderController {
     public Result<OrderStatisticsVO> statistics(){
         OrderStatisticsVO orderStatistics = orderService.getOrderStatistics();
         return Result.success(orderStatistics);
+    }
+    @GetMapping("/details/{id}")
+    @ApiOperation("查看订单详情")
+    public Result<OrderVO> details(@PathVariable Integer id){
+        OrderVO details = orderService.details(id);
+        return Result.success(details);
     }
 }
