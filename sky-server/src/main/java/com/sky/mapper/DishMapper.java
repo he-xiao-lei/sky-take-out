3d1c52f6c0fc0a894947491ef3afb78760026e43
vblaws <h3239498031@gmail.com>
2025-12-14 13:54:29 +0800

    add:工作台部分代码

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -51,4 +51,11 @@ public interface DishMapper {
     List<Dish> list(Dish dish);
     @Update("update dish set status = #{status} where id = #{id}")
     void updateStatus(Integer status, Long id);
+    
+    /**
+     * 根据条件统计菜品数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -10,6 +10,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface SetmealMapper {
@@ -39,4 +40,11 @@ public interface SetmealMapper {
     List<DishItemVO> getDishItemBySetmealId(Long setmealId);
     
     List<Setmeal> list(Setmeal setmeal);
+    
+    /**
+     * 根据条件统计套餐数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
